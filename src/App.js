2dc7ebe07a6aca643f52637002131766f6e1fa1f
Al Nahian Ark <63243119+arknahian@users.noreply.github.com>
2020-08-23 22:16:24 +0600

    something I have leaned from React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,122 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  const products = [
+    {name: "Asfon", price: "$1200" },
+    {name: "AsCom", price: "$2000"},
+    {name:"E-Com", price:"$3000"}
+  ]
+  const businessman = [
+    "Al Nahian Ark",
+    "Bill Gates",
+    "Mark Zuckerberg",
+    "Jeff Bezos",
+    "Elon Musk",
+    "Larry Elison",
+    "Larry Page"
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        
+
+        <ul>
+          {
+            businessman.map(richestPerson => <li>{richestPerson}</li>)
+          }
+        </ul>
+
+          <Counter></Counter>
+
+          <Users></Users>
+
+
+        {
+          products.map(pd => <Product product={pd}></Product> )
+        }
+  
+    
+
+
+
+
+
+
       </header>
     </div>
+
+    
+
   );
+        function Users(){
+          const [users, setUsers] = useState([]);
+          useEffect(() =>{
+            fetch("https://jsonplaceholder.typicode.com/users")
+            .then(res => res.json())
+            .then(data => setUsers(data))
+          }, [])
+          return(
+            <div>
+              <h3>Dynamic Users {users.length}</h3>
+             <ul>
+               {
+                 users.map(user => <li>{user.name} </li>)
+               }
+             </ul>
+              </div>
+          )
+        }
+
+
+  function Counter(){
+
+    const [count, setCount] = useState(0);
+    const handleIncrease = () => {
+     const newCount = count + 1;
+      setCount(newCount)
+    }
+    const handleDecrease = () => {
+      const newCount = count - 1;
+      setCount(newCount)
+    }
+    return(
+      <div>
+        <h1>Count :{count} </h1>
+        <button onClick={handleIncrease}>Increase</button>
+        <button onClick={handleDecrease}> Decrease</button>
+        </div>
+    )
+  }
+  
+  function Product(props) {
+    const productStyle= {
+      backgroundColor: "gray",
+      margin : "10px",
+      float : "left",
+      padding : "10px",
+      width : "400px",
+      height : "400px",
+      border : "1px solid red",
+      borderRadius : "5px",
+      color: "black",
+    }
+    console.log(props);
+    return (
+      <div style={productStyle}> 
+        <h1>{props.product.name}</h1>
+    <h3>Price {props.product.price}</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <button style={{backgroundColor:"black", color:"white", width:"100px", height:"40px", border:"1px solid black", borderRadius:"5px"}}>Buy Now!</button>
+      </div>
+    )
+  }
 }
+ 
 
 export default App;
